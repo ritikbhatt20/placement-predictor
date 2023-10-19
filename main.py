d2d0ee3c2974f84a3f24db93bf9c09e8871d9e77
ritik <ritikbhatt020@gmail.com>
2023-10-19 17:46:12 +0530

    Added changes

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
-from flask import Flask, jsonify
+# importing flask module
+import pickle
+
+import numpy as np
+from flask import Flask, request, jsonify
+
+model = pickle.load(open('model.pkl', 'rb'))
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return jsonify({"msg": "hello deploy from flask glitch"})
+# creating routes
+@app.route('/')
+def home():
+    return "Hello World"
 
 
-if __name__ == "__main__":
-    app.run()
+# api will exist in this route
+# api will handle post request, there are 2 types of request get and post
+# in get, we take input through url... In post without url
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    cgpa = request.form.get('cgpa')
+    iq = request.form.get('iq')
+    profile_score = request.form.get('profile_score')
+
+    # forming dictionary of result
+    # result = {'cgpa': cgpa, 'iq': iq, 'profile_score': profile_score}
+
+    # this is input
+    input_query = np.array([[cgpa, iq, profile_score]], dtype=np.float64)
+
+    # this is output will be in the form of array
+    result = model.predict(input_query)[0]
+
+    # returning in the form of json
+    return jsonify({'placement': str(result)})
+
+
+# Postman jo h vo http request mar rha h.. and flask api is returning the dictioning in form of json
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
